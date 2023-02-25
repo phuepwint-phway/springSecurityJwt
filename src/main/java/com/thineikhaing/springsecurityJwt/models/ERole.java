028214964108485b86cc586966b88889e0084828
thineikhaing <thineikhaing@gmail.com>
2023-02-25 13:29:14 +0800

    setup project | implement model classes for User and Role

---
+++ src/main/java/com/thineikhaing/springsecurityJwt/models/ERole.java
@@ -0,0 +1,7 @@
+package com.thineikhaing.springsecurityJwt.models;
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_MANAGER,
+    ROLE_ADMIN
+}
---
+++ src/main/java/com/thineikhaing/springsecurityJwt/models/Role.java
@@ -0,0 +1,22 @@
+package com.thineikhaing.springsecurityJwt.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "uuid2")
+    @Column(length = 36, nullable = false, updatable = false)
+    private String id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
+}
