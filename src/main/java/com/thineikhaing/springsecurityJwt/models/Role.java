8e79218bb4bd7eb976225bc1f84f48f4b34928ad
ThinEi <thineikhaing@gmail.com>
2023-02-26 20:30:54 +0800

    refactor getter | setter methods

--- src/main/java/com/thineikhaing/springsecurityJwt/models/Role.java
+++ src/main/java/com/thineikhaing/springsecurityJwt/models/Role.java
@@ -27,19 +27,4 @@ public class Role {
         this.name = name;
     }
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public ERole getName() {
-        return name;
-    }
-
-    public void setName(ERole name) {
-        this.name = name;
-    }
 }
