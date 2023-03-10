ecefff2fb85d529aac04fd7b4f958e4030e2cc7e
ThinEi <thineikhaing@gmail.com>
2023-03-10 14:39:47 +0800

    add frontend-maven-plugin - to build web app when spring boot app start

---
+++ src/main/resources/static/precache-manifest.2908503f1c6c0836416260b052ba08bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "60828339a0ffb420f901",
+    "url": "/static/js/runtime~main.60828339.js"
+  },
+  {
+    "revision": "6f3f594cb2e83e18c79f",
+    "url": "/static/js/main.6f3f594c.chunk.js"
+  },
+  {
+    "revision": "b0f1bacebf2bc5ad40fb",
+    "url": "/static/js/2.b0f1bace.chunk.js"
+  },
+  {
+    "revision": "9df7b8f4a82597eefd5c",
+    "url": "/static/js/1.9df7b8f4.chunk.js"
+  },
+  {
+    "revision": "6f3f594cb2e83e18c79f",
+    "url": "/static/css/main.285142d4.chunk.css"
+  },
+  {
+    "revision": "b0f1bacebf2bc5ad40fb",
+    "url": "/static/css/2.e0387745.chunk.css"
+  },
+  {
+    "revision": "151ca4b260ed8d7674872c67c6705907",
+    "url": "/index.html"
+  }
+];
